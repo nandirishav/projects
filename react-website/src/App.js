917217c83ebfb2dd33527c4dc8fbc857154f5b7c
Rishav Nandi <65340002+nandirishav@users.noreply.github.com>
2021-04-14 01:23:10 +0530

    added: button and navbar

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World !</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
